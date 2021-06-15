642f07f9f7fcefb942b68d60a4333fd7b93c4738
unknown <nightsta1kerhf@gmail.com>
2021-06-15 12:56:45 +0300

    v.0.0.1.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import Btn from "./header/btn";
-import "./header/btn.module.css";
+import Btn from './header/btn';
+import './header/btn.module.css';
 
-const App = () => {
-  return (
-    <>
-      <Btn title="React Home Work Phonebook 02" />
-    </>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './components/ContactForm';
+// import ContactList from './components/ContactList';
+// import Filter from './components/Filter';
+
+// class ContactForm extends Component {
+
+// }
+
+class App extends Component {
+  render() {
+    // state = {
+    //   contacts: [
+    //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    //   ],
+    //   filter: '',
+    // };
+
+    return (
+      <>
+        <Btn title="React Home Work Phonebook 02" />
+        <ContactForm />
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import style from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    contact: [],
+    name: '',
+    // number: '',
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <div className={style.section}>
+        <label className={style.label}>
+          Name
+          <input
+            className={style.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={name}
+          />
+        </label>
+
+        <label className={style.label}>
+          Number
+          <input
+            className={style.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={number}
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
